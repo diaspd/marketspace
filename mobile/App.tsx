398b4d5399f53faded2d03082150494148f7245d
Pedro Dias <pdr050203@gmail.com>
2024-07-10 21:35:44 -0300

    add project font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
+import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({Karla_400Regular, Karla_700Bold})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {isFontsLoaded ? <Text>Loaded</Text> : <Text>Not loaded</Text>}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
