f07120a43ff59482f80dd6e0cdd4ae1ebb137a02
Pedro Dias <pdr050203@gmail.com>
2024-07-16 19:03:06 -0300

    style: base home header layout

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,7 +1,25 @@
-import { Text } from "native-base";
+import { Avatar } from "@components/Avatar";
+import { Button } from "@components/Button";
+import { Center, Text, Box, Flex, Heading, VStack } from "native-base";
 
 export function Home() {
   return (
-    <Text>Home</Text>
+    <Box px="6" mt="16">
+      <Flex flexDirection="row">
+        <Avatar size={45} alt="" borderWidthsize={2} />
+
+        <VStack flex={1} ml="2" mt="0.5">
+          <Text fontSize="sm">Boas vindas,</Text>
+          <Heading fontSize="sm">Maria!</Heading>
+        </VStack>
+
+        <Box>
+          <Button 
+            title="Criar anúncio" 
+            variant="terciary"
+          />
+        </Box>
+      </Flex>
+    </Box>
   )
 }
