181c859345c315d3d69fefecf49f1f493bb3cecf
Pedro Dias <pdr050203@gmail.com>
2024-08-15 01:23:33 -0300

    ci: add interceptors

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: 'http://192.168.0.107:3333'
 }) as AxiosInstance;
 
+api.interceptors.response.use((response) => {
+  console.log("INTERCEPTOR =>", response)
+  return response
+}, (error) => {
+  console.log('INTERCEPTOR RESPONSE ERROR =>', error)
+  return Promise.reject(error);
+})
+
 export { api }
