1fae4e3d5c7b20a45593b19f76223f3a0c2418e7
Pedro Dias <pdr050203@gmail.com>
2024-08-19 17:43:52 -0300

    ci: add redirect after user login :tada:

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { useTheme } from "native-base";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
@@ -6,12 +7,15 @@ import { AppRoutes } from "./app.routes";
 import { useAuth } from '@hooks/useAuth';
 
 export function Routes() {
+  const { colors } = useTheme();
   const { user } = useAuth();
-  console.log("Usuário LOGADO ===>", user)
-  
+
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[600]
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
+    <NavigationContainer theme={theme}>
+      {user.id ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
