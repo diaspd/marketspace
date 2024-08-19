9438eeb3e4e7a0d77c3970f8ddeeba9722c55605
Pedro Dias <pdr050203@gmail.com>
2024-08-19 17:11:47 -0300

    ci: add hook useAuth

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,7 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
+import { useAuth } from '@hooks/useAuth';
+
 export function Routes() {
+  const { user } = useAuth();
+  console.log("Usuário LOGADO ===>", user)
+  
   return (
     <NavigationContainer>
       <AuthRoutes />
