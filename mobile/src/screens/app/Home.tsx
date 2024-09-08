5e5e1c413145397b141c879051e16ea168c82cd7
Pedro Dias <pdr050203@gmail.com>
2024-09-08 14:11:58 -0300

    feat: close filter after search

--- mobile/src/screens/app/Home.tsx
+++ mobile/src/screens/app/Home.tsx
@@ -72,6 +72,7 @@ export function Home() {
       );
 
       setProduct(productsData.data);
+      onClose()
     } catch (error) {
       const isAppError = error instanceof AppError;
       const title = isAppError
